9cf6aeed1b88c7017e053b475eb333993b7a3b2d
shooin13 <shooin13@gmail.com>
2024-05-24 00:36:34 +0300

    - removed navigation controller - added showing modal window instead - changed properties of image in SingleImageViewController

--- ImageFeed/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController.swift
@@ -6,6 +6,9 @@ final class SingleImageViewController: UIViewController {
   
   @IBOutlet private weak var imageView: UIImageView!
   
+  @IBAction func didTapBackButton(_ sender: Any) {
+    dismiss(animated: true, completion: nil)
+  }
   override func viewDidLoad() {
     super.viewDidLoad()
     imageView.image = image
